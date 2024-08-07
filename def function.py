690151f6232793db95ab455b3f0475548ad04550
Lihan-Sin <htmlcss0707@gmail.com>
2024-08-07 14:06:18 +0600

    Renamed every file & and some edit

--- def function.py
+++ def function.py
@@ -1,13 +1,13 @@
 def namata():
-     number = float(input("Enter a number: "))
-     for x in range(1, 11):
-         print(number, "*", x, "=", number*x)
+    number = float(input("Enter a number: "))
+    for x in range(1, 11):
+        print(number, "*", x, "=", number*x)
 
- ask = input("Do you want to multiply a number?: ")
- if ask.lower() != "yes":
+ask = input("Do you want to multiply a number?: ")
+if ask.lower() != "yes":
      quit()
- else:
-namata()
+else:
+    namata()
 
 
 
